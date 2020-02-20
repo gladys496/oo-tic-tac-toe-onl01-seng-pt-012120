55fde68c57aa6ffb36f22576a8ced346d688044c
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-20 18:49:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,13 +11,7 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
  
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+ 
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
